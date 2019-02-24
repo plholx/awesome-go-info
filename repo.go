caac4d7e5c5054a024afcef7bdfd386d51f6ca3e
plholx <plholx@126.com>
2019-02-24 21:33:51 +0800

    添加保存仓库信息方法

--- repo.go
+++ repo.go
@@ -43,7 +43,7 @@ func parseReadmeFile()  {
 	}
 	defer file.Close()
 }
-
+//GoRepo github项目仓库信息结构体
 type GoRepo struct {
 	Id int64 `json:"id,omitempty"`
 	ParentId int64 `json:"parent_id,omitempty"`
@@ -78,9 +78,18 @@ func GetDB() (*sql.DB) {
 	return db
 }
 
-//操作数据库
-func opDb()  {
-
+//保存goRepo
+func InsertGoRepo(goRepo *GoRepo)  {
+	//goRepo := new(GoRepo)
+	db := GetDB()
+	sqlStr := `insert into bookmark 
+				(id, parent_id, repo_name, repo_full_name, repo_owner, repo_html_url, repo_description, repo_created_at, repo_pushed_at, repo_homepage, repo_size, repo_forks_count, repo_stargazers_count, repo_subscribers_count, repo_open_issues_count, repo_license_name, repo_license_spdx_id, repo_license_url, add_time, modify_time, repo) 
+				values 
+				(DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20) 
+				RETURNING id`
+	stmt, _ := db.Prepare(sqlStr)
+	defer stmt.Close()
+	stmt.QueryRow(goRepo.ParentId, goRepo.RepoName, goRepo.RepoFullName, goRepo.RepoOwner, goRepo.RepoHtmlURL, goRepo.RepoDescription, goRepo.RepoCreatedAt, goRepo.RepoPushedAt, goRepo.RepoHomepage, goRepo.RepoSize, goRepo.RepoForksCount, goRepo.RepoStargazersCount, goRepo.RepoSubscribersCount, goRepo.RepoOpenIssuesCount, goRepo.RepoLicenseName, goRepo.RepoLicenseSpdxId, goRepo.RepoLicenseURL, goRepo.AddTime, goRepo.ModifyTime, goRepo.Repo).Scan(goRepo.Id)
 }
 func main() {
 	downloadReadmeFile()
