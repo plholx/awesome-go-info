6b0436ba7fcc908902a35f9d96da68e7bf7fa7a1
plholx <plholx@126.com>
2019-02-25 14:58:43 +0800

    添加获取github仓库信息并保存至数据库方法

--- repo.go
+++ repo.go
@@ -3,13 +3,16 @@ package main
 import (
 	"bufio"
 	"database/sql"
+	"encoding/json"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
 )
 
 const (
@@ -22,6 +25,7 @@ func downloadReadmeFile()  {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer res.Body.Close()
 	bytes, e := ioutil.ReadAll(res.Body)
 	if e != nil {
 		log.Fatal(err)
@@ -83,18 +87,84 @@ func GetDB() (*sql.DB) {
 }
 
 //保存goRepo
-func InsertGoRepo(goRepo *GoRepo)  {
+func SaveGoRepo(goRepo *GoRepo)  {
 	//goRepo := new(GoRepo)
 	db := GetDB()
-	sqlStr := `insert into bookmark 
-				(id, parent_id, repo_name, repo_full_name, repo_owner, repo_html_url, repo_description, repo_created_at, repo_pushed_at, repo_homepage, repo_size, repo_forks_count, repo_stargazers_count, repo_subscribers_count, repo_open_issues_count, repo_license_name, repo_license_spdx_id, repo_license_url, add_time, modify_time, repo, category, name, description, homepage) 
+	sqlStr := `insert into go_repo 
+				(id, parent_id, repo_name, repo_full_name, repo_owner, repo_html_url, repo_description, repo_created_at, repo_pushed_at, repo_homepage, repo_size, repo_forks_count, repo_stargazers_count, repo_subscribers_count, repo_open_issues_count, repo_license_name, repo_license_spdx_id, repo_license_url, repo, category, name, description, homepage) 
 				values 
-				(DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24) 
+				(DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22) 
 				RETURNING id`
 	stmt, _ := db.Prepare(sqlStr)
 	defer stmt.Close()
-	stmt.QueryRow(goRepo.ParentId, goRepo.RepoName, goRepo.RepoFullName, goRepo.RepoOwner, goRepo.RepoHtmlURL, goRepo.RepoDescription, goRepo.RepoCreatedAt, goRepo.RepoPushedAt, goRepo.RepoHomepage, goRepo.RepoSize, goRepo.RepoForksCount, goRepo.RepoStargazersCount, goRepo.RepoSubscribersCount, goRepo.RepoOpenIssuesCount, goRepo.RepoLicenseName, goRepo.RepoLicenseSpdxId, goRepo.RepoLicenseURL, goRepo.AddTime, goRepo.ModifyTime, goRepo.Repo, goRepo.Category, goRepo.Name, goRepo.Description, goRepo.Homepage).Scan(goRepo.Id)
+	stmt.QueryRow(goRepo.ParentId, goRepo.RepoName, goRepo.RepoFullName, goRepo.RepoOwner, goRepo.RepoHtmlURL, goRepo.RepoDescription, goRepo.RepoCreatedAt, goRepo.RepoPushedAt, goRepo.RepoHomepage, goRepo.RepoSize, goRepo.RepoForksCount, goRepo.RepoStargazersCount, goRepo.RepoSubscribersCount, goRepo.RepoOpenIssuesCount, goRepo.RepoLicenseName, goRepo.RepoLicenseSpdxId, goRepo.RepoLicenseURL, goRepo.Repo, goRepo.Category, goRepo.Name, goRepo.Description, goRepo.Homepage).Scan(goRepo.Id)
+}
+//获取github仓库信息并保存
+func GetRepoInfoAndSave() {
+	apiURL := strings.Replace(githubReposAPI, ":owner/:repo", "avelino/awesome-go", -1)
+	res, err := http.Get(apiURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	bytes, e := ioutil.ReadAll(res.Body)
+	if e != nil {
+		log.Fatal(err)
+	}
+	var v interface{}
+	json.Unmarshal(bytes, &v)
+	if repoMap, ok := v.(map[string]interface{}); ok {
+		repo := &GoRepo{
+			ParentId:0, 
+			RepoName:repoMap["name"].(string), 
+			RepoFullName:repoMap["full_name"].(string), 
+			RepoHtmlURL:repoMap["html_url"].(string), 
+			RepoDescription:repoMap["description"].(string), 
+			RepoHomepage:repoMap["homepage"].(string), 
+			RepoForksCount:int64(repoMap["forks_count"].(float64)), 
+			RepoStargazersCount:int64(repoMap["stargazers_count"].(float64)), 
+			RepoSubscribersCount:int64(repoMap["subscribers_count"].(float64)), 
+			RepoOpenIssuesCount:int64(repoMap["open_issues_count"].(float64)), 
+			Repo:true, 
+			Category:false, 
+			Name:repoMap["name"].(string), 
+			Description:repoMap["description"].(string), 
+			Homepage:repoMap["homepage"].(string),
+		}
+		//作者信息
+		if ownerMap, ok := repoMap["owner"].(map[string]interface{}); ok {
+			repo.RepoOwner = ownerMap["login"].(string)
+		}
+		//证书信息
+		if licenseMap, ok := repoMap["license"].(map[string]interface{}); ok {
+			repo.RepoLicenseName = licenseMap["name"].(string)
+			repo.RepoLicenseSpdxId = licenseMap["spdx_id"].(string)
+			repo.RepoLicenseURL = licenseMap["url"].(string)
+		}
+		//日期时间信息
+		if createAtStr, ok := repoMap["created_at"].(string); ok {
+			createAt, err := time.Parse(time.RFC3339, createAtStr)
+			if err == nil {
+				repo.RepoCreatedAt = createAt
+			}
+		}
+		if pushedAtStr, ok := repoMap["pushed_at"].(string); ok {
+			pushedAt, err := time.Parse(time.RFC3339, pushedAtStr)
+			if err == nil {
+				repo.RepoPushedAt = pushedAt
+			}
+		}
+		//size可能带小数
+		if sizeFloat64, ok := repoMap["size"].(float64); ok {
+			size := math.RoundToEven(sizeFloat64);
+			repo.RepoSize = int64(size)
+		}
+		SaveGoRepo(repo)
+	}
+
 }
 func main() {
-	downloadReadmeFile()
+	GetRepoInfoAndSave()
+	// createAt, _ := time.Parse(time.RFC3339, "2014-07-06T13:42:15Z")
+	// log.Println(createAt)
 }
