ccdb357d11cb68cd0ef8f1d518e063b080fa87e5
plholx <plholx@126.com>
2019-03-04 11:07:10 +0800

    测试git命令

--- repo.go
+++ repo.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"awesome-go-data/api"
 	"flag"
 	"log"
+	"os/exec"
+
+	//"awesome-go-data/api"
+)
+
+// memory usage optimizations
+const (
+	git = `git`
+	add = `add`
+	spot = `.`
+	commit = `commit`
+	m = `-m`
+	msg = `测试git命令`
+	push = `push`
 )
 
 var accessToken = flag.String("t", "", "GitHub API access_token, 必须输入")
@@ -14,9 +27,26 @@ func main() {
 		log.Fatal("GitHub API access_token为空")
 	}
 	//获取avelino/awesome-go项目中最新的README.md文件
-	readmeFilePath := api.DownloadReadmeFile()
+	//readmeFilePath := api.DownloadReadmeFile()
 	//解析awesome-go中的README.md文件,并存入数据库中
-	api.ParseReadmeFile(*accessToken, readmeFilePath)
+	//api.ParseReadmeFile(*accessToken, readmeFilePath)
 
 	//api.GenerateMd()
+	out, err := exec.Command(git, add, spot).Output()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(out))
+
+	out, err = exec.Command(git, commit, m, msg).Output()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(out))
+
+	out, err = exec.Command(git, push).Output()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(out))
 }
